7e32094ead7c267f8af8643a35422fc19a3bc8c4
Yuan Fei <fei.yuan38@gmail.com>
2017-12-29 23:57:21 -0800

    Exploratory Analysis commit

---
+++ Exploratory Analysis.R
@@ -0,0 +1,54 @@
+rm(list = ls())
+
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(ggplot2)
+library(mice)
+
+datapath <- "C:/Users/yfei/Desktop/Kaggle/Titanic/data"
+
+# Read in the data
+training <- read.csv(file.path(datapath, "train.csv"), stringsAsFactors = FALSE) %>%
+  mutate(type = "Training")
+test <- read.csv(file.path(datapath, "test.csv"), stringsAsFactors = FALSE) %>%
+  mutate(type = "Test")
+
+#### Data exploration
+stacked <- bind_rows(training, test)
+
+## Survival by Age, Sex
+survival_age <- training %>%
+  group_by(Age) %>%
+  summarize(perc = mean(Survived))
+
+survival_sex <- training %>%
+  group_by(Sex) %>%
+  summarize(perc = mean(Survived))
+
+survival_cabin <- training %>%
+  group_by(Cabin) %>%
+  summarize(perc = mean(Survived))
+
+survival_class <- training %>%
+  group_by(Pclass) %>%
+  summarize(perc = mean(Survived))
+
+survival_class <- training %>%
+  group_by(Pclass, Cabin) %>%
+  summarize(count = n())
+
+### Clean Data
+# Create family name variable
+stacked <- stacked %>%
+  mutate(Family_Name = sapply(Name, function(x) str_split(x, ",")[[1]][1]))
+
+# Impute missing ages
+age_impute <- stacked %>%
+  filter(!is.na(Age))
+
+missing_ages <- stacked %>%
+  filter(is.na(Age))
+
+#### Models
+## Logistic regression
